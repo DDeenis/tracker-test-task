c77043be2883e2e54ee8f25cb08a2e0253db29e0
DDeenis <ddeenis46@gmail.com>
2021-06-02 18:33:35 +0300

    data persistanse

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,16 @@
-import { combineReducers, createStore } from "redux";
+import { createStore } from "redux";
+import persistCombineReducers from "redux-persist/es/persistCombineReducers";
+import persistStore from "redux-persist/es/persistStore";
+import storage from 'redux-persist/lib/storage';
 import trackerReducer from "./trackerReducer";
 
-const reducers = combineReducers({
-    tracker: trackerReducer
-})
+const persistConfig = {
+    key: 'trackers',
+    storage,
+    whitelist: ['tracker']
+};
+
+const reducers = persistCombineReducers(persistConfig, { tracker: trackerReducer })
 
 export const store = createStore(reducers);
+export const persistor = persistStore(store);
